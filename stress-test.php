4768b2bea82a86982cd7fe2ee12289d7b0ceafa2
Ali Haider <73309177+alihdr000@users.noreply.github.com>
2022-09-28 17:17:40 +0500

    changes

--- stress-test.php
+++ stress-test.php
@@ -3,62 +3,136 @@
         <div class="container">
             <div class="row">
                 <table cellspacing="0" cellpadding="0" class="table">
+                    <thead>
+                    <tr>
+                        <th>Table</th>
+                        <th>Table</th>
+                        <th>Table</th>
+                        <th>Table</th>
+                        <th>Table</th>
+                        <th>Table</th>
+                    </tr>
+                    </thead>
                     <tbody>
                     <tr>
                         <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"></td>
                         <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                    </tr>
-                    <tr>
-                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"></td>
                         <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
                     </tr>
                     <tr>
                         <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault11" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault22"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault5"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault6"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault1" id="flexRadioDefault7"></td>
                     </tr>
                     <tr>
                         <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault1"></td>
                     </tr>
                     <tr>
                         <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault2" ></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault2" ></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault3" id="flexRadioDefault1"></td>
                     </tr>
                     <tr>
                         <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault4" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault4" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault4" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault4" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault4" id="flexRadioDefault1"></td>
                     </tr>
                     <tr>
                         <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></td>
-                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault5" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault5" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault5" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault5" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault5" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault6" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault6" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault6" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault6" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault6" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault7" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault7" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault7" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault7" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault7" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault8" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault8" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault8" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault8" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault8" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault9" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault9" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault9" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault9" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault9" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault10" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault10" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault10" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault10" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault10" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault11" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault11" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault11" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault11" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault11" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault12" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault12" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault12" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault12" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault12" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault13" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault13" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault13" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault13" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault13" id="flexRadioDefault1"></td>
+                    </tr>
+                    <tr>
+                        <td>Været irritabel og oplevet en indre modstand/modvilje?</td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault14" id="flexRadioDefault1" checked></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault14" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault14" id="flexRadioDefault1"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault14" id="flexRadioDefault2"></td>
+                        <td><input class="form-check-input" type="radio" name="flexRadioDefault14" id="flexRadioDefault1"></td>
                     </tr>
                     </tbody>
                 </table>
