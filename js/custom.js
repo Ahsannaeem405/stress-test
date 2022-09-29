90c3df79bcb02d955199aa934beeb470716237a0
Ali Haider <73309177+alihdr000@users.noreply.github.com>
2022-09-29 13:11:21 +0500

    changes

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,19 @@
 let slider = document.getElementById("rangeSlider");
 let outputEl = document.querySelector(".range-slider__value");
+let slider2 = document.getElementById("rangeSlider2");
+let outputEl2 = document.querySelector(".range-slider__value2");
+let slider3 = document.getElementById("rangeSlider3");
+let outputEl3 = document.querySelector(".range-slider__value3");
+let slider4 = document.getElementById("rangeSlider4");
+let outputEl4 = document.querySelector(".range-slider__value4");
+let slider5 = document.getElementById("rangeSlider5");
+let outputEl5 = document.querySelector(".range-slider__value5");
+let slider6 = document.getElementById("rangeSlider6");
+let outputEl6 = document.querySelector(".range-slider__value6");
+let slider7 = document.getElementById("rangeSlider7");
+let outputEl7 = document.querySelector(".range-slider__value7");
+let slider8 = document.getElementById("rangeSlider8");
+let outputEl8 = document.querySelector(".range-slider__value8");
 
 function decimalSeparator(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -11,23 +25,25 @@ function decimalSeparator(number) {
 slider.oninput = function () {
     outputEl.textContent = `${decimalSeparator(this.value)} `;
 };
+slider2.oninput = function () {
+    outputEl2.textContent = `${decimalSeparator(this.value)} `;
+};
+slider3.oninput = function () {
+    outputEl3.textContent = `${decimalSeparator(this.value)} `;
+};
+slider4.oninput = function () {
+    outputEl4.textContent = `${decimalSeparator(this.value)} `;
+};
+slider5.oninput = function () {
+    outputEl5.textContent = `${decimalSeparator(this.value)} `;
+};
+slider6.oninput = function () {
+    outputEl6.textContent = `${decimalSeparator(this.value)} `;
+};
+slider7.oninput = function () {
+    outputEl7.textContent = `${decimalSeparator(this.value)} `;
+};
+slider8.oninput = function () {
+    outputEl8.textContent = `${decimalSeparator(this.value)} `;
+};
 
-// Pagination
-
-var items = $(".list-wrapper .list-item");
-var numItems = items.length;
-var perPage = 4;
-
-items.slice(perPage).hide();
-
-$('#pagination-container').pagination({
-    items: numItems,
-    itemsOnPage: perPage,
-    prevText: "&laquo;",
-    nextText: "&raquo;",
-    onPageClick: function (pageNumber) {
-        var showFrom = perPage * (pageNumber - 1);
-        var showTo = showFrom + perPage;
-        items.hide().slice(showFrom, showTo).show();
-    }
-});
--- live-test.php
+++ live-test.php
@@ -13,25 +13,89 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Været træt og uoplagt?</td>
+                            <td>Med din personlige udvikling?</td>
                             <td>
                                 <div class="range-slider">
                                     <div class="range-slider__slider">
-                                        <p class="range-slider__value">1</p>
+                                        <p class="range-slider__value m-0 fw-bold">1</p>
                                         <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider"/>
                                     </div>
                                 </div>
                             </td>
                         </tr>
                         <tr>
-                            <td>Været træt og uoplagt?</td>
+                            <td>Med dit parforhold/dit kærlighedsliv?</td>
                             <td>
-                                <label for="customRange1" class="form-label">Example range</label>
-                                <input type="range"  min="0" max="9" value="1"  class="form-range" id="customRange1">
                                 <div class="range-slider">
                                     <div class="range-slider__slider">
-                                        <p class="range-slider__value">1</p>
-                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider"/>
+                                        <p class="range-slider__value2 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider2"/>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Med din familie/dine venner?</td>
+                            <td>
+                                <div class="range-slider">
+                                    <div class="range-slider__slider">
+                                        <p class="range-slider__value3 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider3"/>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Med dine børn?</td>
+                            <td>
+                                <div class="range-slider">
+                                    <div class="range-slider__slider">
+                                        <p class="range-slider__value4 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider4"/>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Med dit arbejdsliv?</td>
+                            <td>
+                                <div class="range-slider">
+                                    <div class="range-slider__slider">
+                                        <p class="range-slider__value5 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider5"/>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Med dit helbred?</td>
+                            <td>
+                                <div class="range-slider">
+                                    <div class="range-slider__slider">
+                                        <p class="range-slider__value6 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider6"/>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Med din fritid/dit fritidsliv? </td>
+                            <td>
+                                <div class="range-slider">
+                                    <div class="range-slider__slider">
+                                        <p class="range-slider__value7 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider7"/>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Med din økonomi/velstand?</td>
+                            <td>
+                                <div class="range-slider">
+                                    <div class="range-slider__slider">
+                                        <p class="range-slider__value8 m-0 fw-bold">1</p>
+                                        <input type="range" min="0" max="9" value="1" class="slider w-100" id="rangeSlider8"/>
                                     </div>
                                 </div>
                             </td>
